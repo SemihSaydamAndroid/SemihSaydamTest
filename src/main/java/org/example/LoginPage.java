c655244992fae5aa0e08b82b6aba4ae98f550be6
semih <semihsaydamz@gmail.com>
2020-02-12 23:53:01 +0300

    Last Commit in wednesday

--- src/main/java/org/example/LoginPage.java
+++ src/main/java/org/example/LoginPage.java
@@ -24,6 +24,8 @@ public class LoginPage {
     }
 
     public void login(String username, String password) {
+        ////_____________________________________________________________________________LOGIN__________________________________________________________________________
+
         webDriver.get("https://www.n11.com/");
         Assert.assertEquals("n11.com - Alışverişin Uğurlu Adresi",webDriver.getTitle());  // Control .get("https://www.n11.com")
         webDriverWait.until(ExpectedConditions.elementToBeClickable(By.className("btnSignIn"))).click();
@@ -36,13 +38,16 @@ public class LoginPage {
         webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton"))).click();
         //webDriver.findElement(By.id("loginButton")).click();   //Click login button
         //webDriver.findElement(By.id("searchData")).sendKeys("Bilgisayar");
-        Assert.assertEquals("n11.com - Alışverişin Uğurlu Adresi",webDriver.getTitle());  //Control; Come back to homepage
+        Assert.assertEquals("n11.com - Alışverişin Uğurlu Adresi",webDriver.getTitle());  // Control to Come back to Home page
+
+        ////________________________________________________________________________________SEARCH______________________________________________________________________
 
         WebElement searchData = webDriver.findElement(By.id("searchData"));
         searchData.sendKeys("Bilgisayar");
         searchData.sendKeys(Keys.ENTER);
         //Bilgisayar yazdığımızı kontrol etme...WebElement searchResultText = webDriver.findElement(By.xpath("//*[@id=\"contentListing\"]/div/div/div[2]/section/div[1]/div[1]/h1"));
 
+        ////________________________________________________________________________________2ND PAGE______________________________________________________________________
 
         webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"contentListing\"]/div/div/div[2]/div[4]/a[2]"))).click();
         //webDriver.findElement(By.xpath("//*[@id=\"contentListing\"]/div/div/div[2]/div[4]/a[2]")).click();
@@ -52,69 +57,60 @@ public class LoginPage {
         Assert.assertTrue("2.sayfaya ulaşılamadı", page.equals("2"));
         ////
 
-        // TODO Random select product
-        //List<WebElement> products = webDriver.findElements(By.className("pro"));
+        ////________________________________________________________________________________RANDOM SELECT_________________________________________________________________
+
+        // TODO Random select product(not working. I select first item because only this clickable)
         List<WebElement> products = webDriver.findElements(By.cssSelector("div[class='pro']"));
         //Random rnd = new Random();
-        //int index = rnd.nextInt(27)+4;   5-32 arası benim listem
+        //int index = rnd.nextInt(27)+4;   5-32 list items
+        // index += 4;
+        //products.get(index).click();
         products.get(5).click();
+
+        ////________________________________________________________________________________BASKET _________________________________________________________________________
 
         WebElement productPrice = webDriver.findElement(By.cssSelector("input[class='productDisplayPrice']"));
         String value = productPrice.getAttribute("value").toString();   // Search page price
 
-        //Click Sepete ekle
-        //webDriver.findElement(By.className("btn btnGrey btnAddBasket")).click();
+        //Click "Sepete ekle" button
         webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[class='btn btnGrey btnAddBasket']"))).click();
 
 
         webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[class='btn btnBlack btnGoBasket']"))).click();  // go to basket
         //webDriver.findElement(By.cssSelector("a[class='btn btnBlack btnGoBasket']")).click();
 
-        // Clear the basket
+        // TODO Clear the basket (not working)____(i want to clear basket because if u have items in there you cant pass Control prize)
         //webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("allItemSelected"))).click();
        //webDriverWait.until(ExpectedConditions.elementToBeClickable(By.className("removeProd svgIcon svgIcon_trash"))).click();
         //
+
+        //________________________________________________________________________CONTROL PRİZE__________________________________________________________________________
 
         WebElement productBasketPrice = webDriver.findElement(By.className("productPrice"));
         String basketValue = productBasketPrice.getAttribute("value").toString();
         //basketValue = "6423.55"; //Test my assertTure command line
         Assert.assertTrue("Sayfadaki fiyat ile Sepetteki fiyat aynı değil!",value.equals(basketValue));
 
-
+        //________________________________________________________________________INCREASE PRODUCT__________________________________________________________________________
         webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[class='spinnerUp spinnerArrow']"))).click(); // increase products to 2
         //Control products pieces
         WebElement productCount = webDriver.findElement(By.className("quantity"));
         String productValue = productCount.getAttribute("value").toString();
         Assert.assertTrue("Lütfen adeti 2 yapınız.",productValue.equals("2"));
 
-        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[title='Sil']"))).click();
+        ////___________________________________________________________________ERASE AND CONTROL "Sepetiniz boş"__________________________________________________________
+
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("span[title='Sil']"))).click();   ///Erase basket item
+
         //Control "Sepetiniz Boş"
         WebElement emptyBasket = webDriver.findElement(By.cssSelector("h2[class='title']"));
         String controlEmpty = emptyBasket.getText().toString();
         Assert.assertTrue("Sepetiniz boş değil",controlEmpty.equals("Sepetiniz Boş")); /// I try "Sepetinizzzz boş" for assertTrue. And it will work correctly
 
 
-
-        //Random rnd = new Random();
-        //int index = rnd.nextInt(26)+1;
-        //List<WebElement> productssName = webDriver.findElements(By.className("productName bold"));
-        //String productName = productssName.get(index).getText();
-        //List<WebElement> productList = webDriver.findElements(By.className("columnContent"));
-       // productList.get(index).click();
-
-
-
-       webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("login"))).click();
-
-
+        webDriver.quit();
     }
 
-    private void openLoginMenu() {
-        WebElement mainLoginButton = webDriver.findElement(By.id("myAccount"));
-        mainLoginButton.click();
-        Actions actions = new Actions(webDriver);
-        actions.moveToElement(mainLoginButton).build().perform();
-    }
 }
 
 
--- src/test/java/org/example/LoginPageTest.java
+++ src/test/java/org/example/LoginPageTest.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.junit.Test;
+import sun.rmi.runtime.Log;
 
 public class LoginPageTest extends PageTest{
 
     @Test
-    public void login() {
-        new LoginPage(driver).login("testiniumtest@gmail.com","testinium123");  // n11 Account
+    public void login(){
+        new LoginPage(driver).login("testiniumtest@gmail.com","testinium123");
     }
+
 }
